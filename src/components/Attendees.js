8cdcccf60637ad7aa9c8e35e37384f79789afe95
Jacob Perry <perryjay123@live.com>
2023-02-26 23:33:48 +0000

    Moved Images and adjusted code

--- src/components/Attendees.js
+++ src/components/Attendees.js
@@ -1,109 +1,123 @@
-import summit_img from '../Brand_Img.png'
-import person_icon from '../person.svg'
-import arrow from '../arrow.svg'
-import plus from '../circle-plus.svg'
-import computer from '../computer.svg'
-import Card from './card'
+import summit_img from "../images/summit.png";
+import person_icon from "../images/person.svg";
+import arrow from "../images/arrow.svg";
+import plus from "../images/circle-plus.svg";
+import computer from "../images/computer.svg";
+import Card from "./card";
 
 export default function Attendees() {
+  const step_2_content = "Start by creating a general registration workflow";
 
-   const step_2_content = "Start by creating a general registration workflow"
+  const registration_cards = [
+    {
+      icon: arrow,
+      title: "Attendee Registration",
+      content: "Start by creating a general registration workflow",
+    },
+    {
+      icon: arrow,
+      title: "Attendee Registration",
+      content: "Start by creating a general registration workflow",
+    },
+    {
+      icon: arrow,
+      title: "Attendee Registration",
+      content: "Start by creating a general registration workflow",
+    },
+  ];
 
-   const registration_cards = [
-      {
-         icon: arrow,
-         title: "Attendee Registration",
-         content: "Start by creating a general registration workflow"
-      },
-      {
-         icon: arrow,
-         title: "Attendee Registration",
-         content: "Start by creating a general registration workflow"
-      },
-      {
-         icon: arrow,
-         title: "Attendee Registration",
-         content: "Start by creating a general registration workflow"
-      },
-   ]
+  return (
+    <section className="attendees">
+      <div className="header">
+        <img className="summit-img rounded-img" src={summit_img} />
+        <div>
+          <h1>RainFocus Summit</h1>
+          <p>December 15th</p>
+          <p>Lehi, Utah</p>
+        </div>
+        <button src="/" type="button">
+          Edit Event
+        </button>
+        <div className="attendee-header-information">
+          <h2>Event setup guide</h2>
+          <p>
+            See the available list of modules below. We suggest that you start
+            with the attendee module.
+          </p>
+        </div>
+      </div>
 
-   return (
-      <section className='attendees'>
-         <div className="header">
-            <img className='summit-img rounded-img' src={summit_img} />
-            <div>
-               <h1>RainFocus Summit</h1>
-               <p>December 15th</p>
-               <p>Lehi, Utah</p>
+      <div className="main-content">
+        <div className="attendee-title">
+          <img src={person_icon} />
+          <h3>Attendee</h3>
+        </div>
+
+        <div className="instructions">
+          <p className="step-title">
+            <span>Step 1: </span>Base settings.
+          </p>
+
+          <div className="fullwidth-card">
+            <div className="borderless-card">
+              <h4>General</h4>
+              <p>Define Attendee types & attributes</p>
             </div>
-            <button src="/" type='button'>Edit Event</button>
-            <div className='attendee-header-information'>
-               <h2>Event setup guide</h2>
-               <p>See the available list of modules below. We suggest that you start with the attendee module.</p>
+            <div className="borderless-card">
+              <h4>Title</h4>
+              <p>
+                Description that explains the value goes here. Description that
+                explains the value goes here.{" "}
+              </p>
             </div>
-         </div>
-
-         <div className='main-content'>
-
-            <div className='attendee-title'>
-               <img src={person_icon} />
-               <h3>Attendee</h3>
+            <div className="borderless-card">
+              <h4>Title</h4>
+              <p>
+                Description that explains the value goes here. Description that
+                explains the value goes here.{" "}
+              </p>
             </div>
+          </div>
 
-            <div className='instructions'>
-               <p className='step-title'><span>Step 1: </span>Base settings.</p>
+          <p className="step-title">
+            <span>Step 2: </span>Build registration workflow
+          </p>
 
-               <div className='fullwidth-card'>
-                  <div className='borderless-card'>
-                     <h4>General</h4>
-                     <p>Define Attendee types & attributes</p>
-                  </div>
-                  <div className='borderless-card'>
-                     <h4>Title</h4>
-                     <p>Description that explains the value goes here. Description that explains the value goes here. </p>
-                  </div>
-                  <div className='borderless-card'>
-                     <h4>Title</h4>
-                     <p>Description that explains the value goes here. Description that explains the value goes here. </p>
-                  </div>
-               </div>
+          {/* Create cards from array */}
+          {registration_cards.map((data) => {
+            return (
+              <Card>
+                <div className="card-header">
+                  <img alt="arrow icon" src={data.icon} />
+                  <h4>{data.title}</h4>
+                </div>
+                <p>{data.content}</p>
+              </Card>
+            );
+          })}
 
-               <p className='step-title'><span>Step 2: </span>Build registration workflow</p>
-
-               {/* Create cards from array */}
-               {registration_cards.map(data => {
-                  return (
-                     <Card>
-                        <div className="card-header">
-                           <img alt="arrow icon" src={data.icon} />
-                           <h4>{data.title}</h4>
-                        </div>
-                        <p>{data.content}</p>
-                     </Card>
-                  )
-               })}
-
-               {/* Add Workflow Card */}
-               <Card className="create-card">
-                  <div className="card-header">
-                     <img alt='Plus icon' src={plus} />
-                  </div>
-                  <p>{"Add Registration Workflow"}</p>
-               </Card>
-               <p className='step-title'><span>Step 3: </span>Design post-registration expereinces.</p>
-               <div className='card-grid'>
-                  <Card>
-                     <div className="card-header">
-                        <img alt="computer icon" src={computer} />
-                        <h4>{"Attendee Registration"}</h4>
-                     </div>
-                     <p>{step_2_content}</p>
-                  </Card>
-               </div>
-               {/* </div> */}
+          {/* Add Workflow Card */}
+          <Card className="create-card">
+            <div className="card-header">
+              <img alt="Plus icon" src={plus} />
             </div>
-
-         </div>
-      </section>
-   )
+            <p>{"Add Registration Workflow"}</p>
+          </Card>
+          <p className="step-title">
+            <span>Step 3: </span>Design post-registration expereinces.
+          </p>
+          <div className="card-grid">
+            <Card>
+              <div className="card-header">
+                <img alt="computer icon" src={computer} />
+                <h4>{"Attendee Registration"}</h4>
+              </div>
+              <p>{step_2_content}</p>
+            </Card>
+          </div>
+          {/* </div> */}
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,49 +1,51 @@
-import search from '../search.svg';
+import search from "../images/search.svg";
 
 export default function Menu() {
-   return (
-      <section className="menu">
-         <div className='top'>
-            <h2>RainFocus Summit</h2>
-            <p>Lehi, UT <span>  •  </span> December 15th</p>
-            <form>
-               <input type="search" placeholder='search'></input>
-               <img src={search} className='search-icon'/>
-            </form>
-         </div>
-         <ul>
-            <li className="link-container">
-               <div className='link' tabIndex={0}>
-                  <img src={''} />
-                  <p>Guide</p>
-               </div>
-            </li>
-            <li className="link-container">
-               <div className='selected link' tabIndex={0}>
-                  <img src={''} />
-                  <p>Attendees</p>
-               </div>
-               <div className='sub-menu-list'>
-                  <a href="/">Attendees </a>
-                  <a href="/">Attendee types </a>
-                  <a href="/">Packages </a>
-                  <a href="/">Reg Codes </a>
-                  <a href="/">Discounts </a>
-               </div>
-            </li>
-            <li className="link-container">
-               <div className='link' tabIndex={0}>
-                  <img src={''} />
-                  <p>Content</p>
-               </div>
-            </li>
-            <li className="link-container">
-               <div className='link' tabIndex={0}>
-                  <img src={''} />
-                  <p>Exhibitors</p>
-               </div>
-            </li>
-         </ul>
-      </section>
-   )
+  return (
+    <section className="menu">
+      <div className="top">
+        <h2>RainFocus Summit</h2>
+        <p>
+          Lehi, UT <span> • </span> December 15th
+        </p>
+        <form>
+          <input type="search" placeholder="search"></input>
+          <img src={search} className="search-icon" />
+        </form>
+      </div>
+      <ul>
+        <li className="link-container">
+          <div className="link" tabIndex={0}>
+            <img src={""} />
+            <p>Guide</p>
+          </div>
+        </li>
+        <li className="link-container">
+          <div className="selected link" tabIndex={0}>
+            <img src={""} />
+            <p>Attendees</p>
+          </div>
+          <div className="sub-menu-list">
+            <a href="/">Attendees </a>
+            <a href="/">Attendee types </a>
+            <a href="/">Packages </a>
+            <a href="/">Reg Codes </a>
+            <a href="/">Discounts </a>
+          </div>
+        </li>
+        <li className="link-container">
+          <div className="link" tabIndex={0}>
+            <img src={""} />
+            <p>Content</p>
+          </div>
+        </li>
+        <li className="link-container">
+          <div className="link" tabIndex={0}>
+            <img src={""} />
+            <p>Exhibitors</p>
+          </div>
+        </li>
+      </ul>
+    </section>
+  );
 }
